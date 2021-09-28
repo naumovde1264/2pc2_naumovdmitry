a277d1a04f0e018ff1ccb70aabf278e4c34557f6
Дмитрий Наумов <dimanaumov.bilain@gmail.com>
2021-09-28 11:53:59 +0300

    pz

--- pz_5/Program.cs
+++ pz_5/Program.cs
@@ -4,9 +4,44 @@ namespace pz_5
 {
     class Program
     {
+
         static void Main(string[] args)
+         
+      
         {
-            Console.WriteLine("Работа с ГИС");
+            string letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            Console.WriteLine("задание №1");
+            int i;
+            for (i = 10; i <= 80; i = i + 4)
+                Console.WriteLine(i);
+            Console.WriteLine();
+
+            Console.WriteLine("задание №2");
+            int j;
+            {
+                for (j = 4; j < 16; j++)
+                    Console.WriteLine(letters[j]);
+
+            }
+            Console.WriteLine("задание №3");
+            int e;
+            for (e = 0; e < 7; e++)
+                Console.WriteLine("#######");
+
+            Console.WriteLine("задание №4");
+            int g;
+            for (g = 0; g < 100; g++)
+                if (g % 5 == 4)
+                    Console.Write(g + " ");
+           
+            
+
+                
+                
+          
+
         }
+        
+           
     }
 }
