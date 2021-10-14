f631caf7ac8c004852cede23169ab3a1995c7c5e
Дмитрий Наумов <dimanaumov.bilain@gmail.com>
2021-10-14 11:17:56 +0300

    /

---
+++ pz_8/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace pz_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int[] arrayInt = new int[10];
+            for (int i = 0; i < arrayInt.Length; i++)
+            {
+                arrayInt[i] = rand.Next(0, 50);
+            }
+            Console.Write("Массив до: ");
+            foreach (int i in arrayInt)
+            {
+                Console.Write($"{i} ");
+            }
+            Console.WriteLine();
+            Array.Sort(arrayInt);
+            Console.Write("Массив после: ");
+            foreach (int i in arrayInt)
+            {
+                Console.Write($"{i} ");
+            }
+        }
+    }
+}
