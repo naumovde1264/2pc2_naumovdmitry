274ff42dec97e33d55032d6b1411df23c9d8f66c
Дмитрий Наумов <dimanaumov.bilain@gmail.com>
2021-10-29 17:22:30 +0300

    .

---
+++ Pz_10/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Pz_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random();
+            var lastItemsArray = new int[10];
+            var maxItemsArray = new int[10];
+
+
+
+            int[][] nums = new int[10][];
+
+            for (int i = 0; i < 10; i++)
+            {
+
+                nums[i] = new int[random.Next(5, 35)];
+
+                for (int j = 0; j < nums[i].Length; j++)
+                {
+                    nums[i][j] = random.Next(100);
+                }
+            }
+
+            foreach (int[] item in nums)
+            {
+                foreach (int i in item)
+                {
+                    Console.Write(i + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < 10; i++)
+            {
+                lastItemsArray[i] = nums[i][nums[i].Length - 1];
+            }
+
+            foreach (var i in lastItemsArray)
+            {
+                Console.Write(i + " ");
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < 10; i++)
+            {
+                maxItemsArray[i] = nums[i].Max();
+            }
+
+            foreach (var i in maxItemsArray)
+            {
+                Console.Write(i + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
