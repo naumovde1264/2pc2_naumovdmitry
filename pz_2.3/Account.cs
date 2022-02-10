5a18a65920e6b9e9cb8e5b286a392af3d29661d8
Дмитрий Наумов <dimanaumov.bilain@gmail.com>
2022-02-10 22:21:42 +0300

    test edit

---
+++ pz_2.3/Account.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pz_2._3
+{
+    class Account
+    {
+        public string login;
+        public int ID;
+        public string email;
+        public string pass;
+        public DateTime date;
+        public Account(string l, int id, string e, string p, DateTime d)
+        {
+            login = l;
+            ID = id;
+            email = e;
+            pass = p;
+            date = d;
+
+
+        }
+        public Account(string l = "unknow", int id = 0, string e = "unknow", string p = "unknow")
+        {
+            login = l;
+            ID = id;
+            email = e;
+            pass = p;
+        }
+        public Account(string l, string e, string p)
+        {
+            login = l;
+
+            email = e;
+            pass = p;
+        }
+        public void GetInfo()
+        {
+            if (date == DateTime.MinValue)
+                Console.WriteLine($"{login} \n{ID} \n{email} \n{pass} ");
+
+            else
+                Console.WriteLine($"{login} \n{ID} \n{email} \n{pass} \n{date}");
+        }
+    }
+}
---
+++ pz_2.3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace pz_2._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime d = new DateTime(2022, 07, 13);
+            Account acc = new Account("salo", 666, "marylinmanson@gmail.com", "MatItoPloho", d);
+            acc.GetInfo();
+            Account acc1 = new Account("salo", 666, "marylinmanson@gmail.com", "MatItoPloho");
+            acc1.GetInfo();
+            Account acc2 = new Account("salo", "marylinmanson@gmail.com", "MatItoPloho");
+            acc2.GetInfo();
+            Account acc3 = new Account("salo", 666, "marylinmanson@gmail.com");
+            acc3.GetInfo();
+        }
+    }
+}
