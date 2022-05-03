d657c529acd5fad24d1b86e74417ba0d0ce2fd49
Дмитрий Наумов <dimanaumov.bilain@gmail.com>
2022-05-03 10:44:15 +0300

    booop

---
+++ PZ.2.10/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+namespace PZ._2._10
+{
+    class Pobeditel_po_Jizni
+    {
+        public int joke;
+    }
+    class Program
+    {
+        unsafe static void Main()
+        {
+            fixed (int* ui = &new Pobeditel_po_Jizni().joke)
+            {
+                char* peremennaya = (char*)ui;
+                *peremennaya = 'A';
+                *(peremennaya + 1) = 'B';
+                Console.WriteLine("Значение поля joke = " + (*ui));
+            }
+        }
+    }
+}
---
+++ pz_2.10/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace pr217_2
+{
+    class MyClass
+    {
+        public int n;
+    }
+    class Program
+    {
+        unsafe static void Main()
+        {
+            fixed (int* p = &new MyClass().n)
+            {
+                char* s = (char*)p;
+                *s = 'A';
+                *(s + 1) = 'B';
+                Console.WriteLine("Значение поля n = " + (*p));
+            }
+        }
+    }
+}
